2286b79c744236d5658665b5dc8b07180a81f8d5
Duy Anh <buithihaiyen1972qt@gmail.com>
2026-07-08 13:26:46 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 console.log("Initializing API Server...");
 import { ProviderManager } from "./server/providers/providerManager";
-import { db } from './db';
+const db = admin.firestore();
 import { HealthMonitor } from "./server/providers/healthMonitor";
 import { CerebrasRotator } from "./server/providers/cerebrasRotator";
 import { CrossProviderRotator } from "./server/providers/crossProviderRotator";
